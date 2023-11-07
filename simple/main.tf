a30f654964d525d3ce4700824670901865a3207c
ravi2krishna <ravi2krishna@gmail.com>
2023-11-07 16:28:23 +0530

    ebs volume

--- simple/main.tf
+++ simple/main.tf
@@ -1,1 +1,26 @@
 # Simple Scenario
+
+# AWS Provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
+
+# EBS Volume
+resource "aws_ebs_volume" "example" {
+  availability_zone = "us-east-2a"
+  size              = 5
+
+  tags = {
+    Name = "tf-cloud-volume"
+  }
+}
